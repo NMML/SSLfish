74a58b50de916d9859fa3b0a734ae2a7bb16411c
Devin Johnson <devin.johnson@noaa.gov>
2013-03-06 15:19:50 -0800

    initial commit 3

--- R/SSLfish-package.R
+++ R/SSLfish-package.R
@@ -1,22 +1,19 @@
-#' R package for fitting temporal trends to abundence data aggregated over large regions when subregions have missing data
+#' R package for simulating effects of fisheries removal of prey on Steller sea lion populations
 #' 
-#' This package fits a log-linear trend models to regions aggregated over sites. The sites may contain missing surveys that 
-#' are not temporally aligned with the missing data at other sites, making direct aggregation impossible. The functions within the package
-#' model the indivdual sites with a semi-parametric (possibly, zero-inflated) model to interpolate missing data from which regional aggregations
-#' can be made. By using Markov Chain Monte Carlo, on can sample from the posterior predictive distribution of the regional aggregations
-#' Then calculate the log-linear trend over the time period of interest as a derived parameter. Using the posterior predictive distribution
-#' allows incorporation of both parameter uncertainty as well as uncertainty due to sampling the local abundance processes.
-#' 
-#' \tabular{ll}{ Package: \tab agTrend\cr Type: \tab Package\cr Version: \tab
-#' 0.1\cr Date: \tab 2013-03-01\cr License: \tab Unlimited\cr LazyLoad: \tab
+#' This package simultaneously simulates a sea lion population, prey base, and fisheries removal of prey. 
+#' The primary goal is to investiagte the validity of approaches used previously in the literature to assess
+#' the effects of fishing on the abundance of Steller sea lions. 
+#'  
+#' \tabular{ll}{ Package: \tab SSLfish \cr Type: \tab Package\cr Version: \tab
+#' 0.1\cr Date: \tab 2013-03-06\cr License: \tab Unlimited\cr LazyLoad: \tab
 #' yes\cr }
 #' 
-#' @name agTrend-package
-#' @aliases agTrend-package agTrend
+#' @name SSLfish-package
+#' @aliases SSLfish-package SSLfish
 #' @docType package
-#' @author Devin S. Johnson
-#' 
+#' @author Devin Johnson and Paul Conn
+#'
 #' Maintainer: Devin S. Johnson <devin.johnson@@noaa.gov>
 #' 
-#' 
+#' @useDynLib SSLfish
 NULL
---
+++ src/sim_code.cpp
@@ -0,0 +1,19 @@
+
+#include <Rcpp.h>
+using namespace Rcpp;
+
+NumericVector sim_ssl(NumericVector prev, NumericMatrix A) {
+    NumericVector z   = NumericVector::create( 0.0, 1.0 ) ;
+    return z ;
+}
+
+NumericVector sim_fish(NumericVector prev, NumericMatrix A) {
+    NumericVector z   = NumericVector::create( 0.0, 1.0 ) ;
+    return z ;
+}
+
+NumericVector sim_all(NumericVector prev, NumericMatrix A) {
+    NumericVector z   = NumericVector::create( 0.0, 1.0 ) ;
+    return z ;
+}
+
