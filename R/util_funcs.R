02679aa126442fc08ee5af6da650dab604f546e8
Devin Johnson <devin.johnson@noaa.gov>
2013-06-28 17:23:44 -0700

    Little bit more work

--- R/util_funcs.R
+++ R/util_funcs.R
@@ -1,5 +1,16 @@
+#inverse logit transform
+invLogit <- function(x){
+  1/(1+exp(-x))
+}
+
+# logit link function
+logit <- function(x){
+  if(any(x<=0) | any(x>=1)) stop("x must lie in (0,1)")
+  log(x/(1-x))
+}
+
 #function to evaluate double logistic function for fishery selectivity
-double_logistic<-function(a,eta1,eta2,alpha1,alpha2){
+double.logistic<-function(a,eta1,eta2,alpha1,alpha2){
   s.a=1/(1+exp(-eta1*(a-alpha1)))*(1-1/(1+exp(-eta2*(a-alpha2))))
   s.a=s.a/max(s.a)
 }
@@ -10,8 +21,10 @@ logistic<-function(a,eta1,alpha1){
   m.a/max(m.a)
 }
 
+BH.curve <- function(R0, h, SSB)
+
 #compute unfished spawning biomass per recruit
-unfished_bpr<-function(Maturity,Weight,M){
+unfished.bpr<-function(Maturity,Weight,M){
   n.age=length(Maturity)
   N=rep(1,n.age)
   for(i in 1:n.age)N[i]=exp(-M*i)
