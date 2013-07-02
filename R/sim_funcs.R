dab17f8976acd0f99bb880087dd1e1c083f26ef4
Devin Johnson <devin.johnson@noaa.gov>
2013-07-02 11:05:57 -0700

    Set up framework for updating populations one year at a time. We'll loop over years with these functions on the inside.

--- R/sim_funcs.R
+++ R/sim_funcs.R
@@ -1,4 +1,18 @@
-ssl.fish.sim <- function(parList, burn1, burn2, iter){
-  nsite <- parList$nsite
+
+###
+### Functions to increment populations by one year
+###
+
+### Sea lions functions
+
+inc.N.ssl <- function(f.list, S.list, N.ssl.list){
+  
+}
+
+inc.S <- function(B.star.list, B.list, param.S.list){
+  
+}
+
+inc.f <- function(B.star.list, B.list, param.f.list){
   
 }
