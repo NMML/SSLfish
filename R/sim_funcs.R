a31d7b4863937dae6d33001129e6e24e724ff073
Devin Johnson <devin.johnson@noaa.gov>
2013-07-02 17:25:43 -0700

    Added A matix generator and the HFYS 2007 data

--- R/sim_funcs.R
+++ R/sim_funcs.R
@@ -3,9 +3,10 @@
 ### Functions to increment populations by one year
 ###
 
-### Sea lions functions
+### Sea lion functions
+library(compiler)
 
-inc.N.ssl <- function(f.list, S.list, N.ssl.list){
+inc.N.ssl <- function(A.list, N.ssl.list){
   
 }
 
@@ -16,3 +17,16 @@ inc.S <- function(B.star.list, B.list, param.S.list){
 inc.f <- function(B.star.list, B.list, param.f.list){
   
 }
+
+make.A.list <- cmpfun(function(f.list, S.list, mal2femRatio){
+  K <- length(f.list)
+  n.age <- length(f.list[[1]])
+  out <- replicate(K, matrix(0,2*n.age,2*n.age), simplify=FALSE)
+  for(i in 1:K){
+    out[[i]][1,1:n.age] <- 0.5*f.list[[i]]*S.list[[i]]
+    out[[i]][2:n.age, 1:(n.age-1)] <- diag(S.list[[i]][-n.age])
+    out[[i]][(n.age+1),1:n.age] <- 0.5*f.list[[i]]*S.list[[i]]
+    out[[i]][(n.age+2):(2*n.age), (n.age+1):(2*n.age-1)] <- diag((mal2femRatio*S.list[[i]])[-n.age])
+  } 
+  return(out)
+})
