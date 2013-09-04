ff9cfe9d246ca4a4499314e85ed9ba0a967ce5a6
Devin Johnson <devin.johnson@noaa.gov>
2013-09-04 15:25:29 -0700

    SSLfish is now a build-able package

--- R/SSLfish-package.R
+++ R/SSLfish-package.R
@@ -5,15 +5,23 @@
 #' the effects of fishing on the abundance of Steller sea lions. 
 #'  
 #' \tabular{ll}{ Package: \tab SSLfish \cr Type: \tab Package\cr Version: \tab
-#' 0.1\cr Date: \tab 2013-03-06\cr License: \tab Unlimited\cr LazyLoad: \tab
+#' 0.1\cr Date: \tab 2013-09-04\cr License: \tab Unlimited\cr LazyLoad: \tab
 #' yes\cr }
 #' 
 #' @name SSLfish-package
 #' @aliases SSLfish-package SSLfish
 #' @docType package
-#' @author Devin Johnson and Paul Conn
+#' @author Paul Conn and Devin Johnson
 #'
 #' Maintainer: Devin S. Johnson <devin.johnson@@noaa.gov>
 #' 
-#' @useDynLib SSLfish
+NULL
+
+#' @name HFYS_appendix_C
+#' @title Data from Holmes et al. (2007) Appendix C
+#' @description Data from Holmes et al. (2007) Appendix C
+#' @docType data
+#' @references Holmes , E., Fritz, L., York, A., and Sweeny, K. (2007) Age-structured modeling 
+#' reveals long-term declines in the natality of western Steller sea lions. Ecological Applications 17:2214-2232.
+#' Repreint available at: \url{http://alaskafisheries.noaa.gov/protectedresources/stellers/esa/biop/draft/holmes_etal_2007_ssl_agestructure.pdf}
 NULL
