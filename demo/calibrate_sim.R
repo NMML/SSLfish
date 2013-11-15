3b79355227bba532a961a8bf9e51ebcc211f5a1d
Devin.Johnson <devin.johnson@AKC3SM1009-DJ13.local>
2013-11-15 11:39:43 -0800

    More minors

--- demo/calibrate_sim.R
+++ demo/calibrate_sim.R
@@ -1,8 +1,3 @@
-require(gtools)
-require(compiler)
-source("c:/users/paul.conn/git/SSLfish/R/util_funcs.R")
-source("c:/users/paul.conn/git/SSLfish/R/sim_funcs.R")
-source("c:/users/paul.conn/git/SSLfish/R/obj_fun_SSLfish.R")
 tsteps.1=150
 tsteps.2=25
 tsteps.3=20
@@ -13,4 +8,4 @@ pars=c(0.104,3.344,0.771,1.298,-0.038,-1.286) #delta, log(q), log(alpha0),log(be
 Out<-optim(par=pars,fn=obj_fun_SSLfish,method="SANN",fixed.vars=fixed.vars)
 print(Out$par)
 summary(Out)
-save(Out,file="sim_anneal_Out.Rdat")
+# save(Out,file="sim_anneal_Out.Rdat")
